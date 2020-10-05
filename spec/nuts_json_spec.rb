afc804f6c48a274f48044e763911aeab96ac38c4
miball0202 <aisacart@gmail.com>
2020-10-05 23:20:07 +0900

    ver 0.1.0

--- spec/nuts_json_spec.rb
+++ spec/nuts_json_spec.rb
@@ -1,9 +1,2 @@
 RSpec.describe NutsJson do
-  it "has a version number" do
-    expect(NutsJson::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
